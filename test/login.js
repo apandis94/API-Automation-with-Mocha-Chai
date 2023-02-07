c28dfcd06277247eb87c7925838fae9399c68c34
apandis94 <apandi.saputra94@gmail.com>
2023-02-07 15:30:09 +0700

    Update Auth

--- test/login.js
+++ test/login.js
@@ -1,14 +1,23 @@
 const request = require("supertest");
 const { expect } = require("chai");
 
+var password = "SUKAJAYA10";
+var email1 = "kelontong.murah@gmail.com";
 var login = {
   name: "Kelontong Murah",
-  email: "kelontong.murah@gmail.com",
-  password: "SUKAJAYA10",
+  email: email1,
+  password: password,
 };
 
+var auth = {
+  email: email1,
+  password: password,
+};
+
+let email;
+let token;
+
 describe("Regist Toko in Casier Aja", () => {
-  let email;
   const response = request("https://kasir-api.belajarqa.com").post("/registration").send(login);
   it("Find Status and Body", async () => {
     console.log((await response).status);
@@ -26,5 +35,20 @@ describe("Regist Toko in Casier Aja", () => {
     expect((await response).body.status).to.equal("success");
     expect((await response).body.message).to.equal("Toko berhasil didaftarkan");
     expect((await response).body.data.name).to.equal("Kelontong Murah");
+  });
+});
+
+describe("Get Auth", () => {
+  const response = request("https://kasir-api.belajarqa.com").post("/authentications").send(auth);
+  it("Find Status and Body", async () => {
+    console.log((await response).status);
+    console.log((await response).body);
+    token = (await response).body.data.accessToken;
+    console.log("-----------//------------");
+    console.log("akses token adalah = " + token);
+  });
+
+  it("Equal Status must be 201", async () => {
+    expect((await response).status).to.equal(201);
   });
 });
